081f3361d0e1c5eb963947b3e2b81d3a4d89e872
manuelcabrerizo <manuelcabrerizo5@gmail.com>
2020-12-21 18:26:24 -0300

    update maps

--- src/main.cpp
+++ src/main.cpp
@@ -27,6 +27,7 @@ tileMap_t map0;
 tileMap_t map1;
 tileMap_t map2;
 tileMap_t* actualMap;
+int actualMapInt = 1;
 
 bool PlayerCanMove(int x, int y)
 {
@@ -70,7 +71,7 @@ bool ChangeMap(int x, int y)
         int x2 = 0;
         for(int x = 0; x < 30; x++)
         {
-            tiles[(y2 * 30) + x2] = actualMap->bottomLayer[((y - 1) * 30) + x];
+            tiles[(y2 * 30) + x2] = actualMap->colitions[((y - 1) * 30) + x];
             x2++;
         }
         y2++;   
@@ -79,11 +80,33 @@ bool ChangeMap(int x, int y)
     int tile1 = tiles[((y / 64) * 30) + ((x + 20) / 64)];
     int tile2 = tiles[((y / 64) * 30) + ((x + 40) / 64)];
 
-    if((tile1 == 2) || (tile2 == 2)) 
+    if(((tile1 == 2) || (tile2 == 2)) || ((tile1 == 3) || (tile2 == 3))) 
     {
         return true;
     }
     return false;
+}
+
+int GetColitionsInt(int x, int y)
+{
+    x -= tileInfo.x;
+    y -= tileInfo.y;
+
+    int tiles[600];
+    int y2 = 0;
+    for(int y = 20; y > 0; y--)
+    {
+        int x2 = 0;
+        for(int x = 0; x < 30; x++)
+        {
+            tiles[(y2 * 30) + x2] = actualMap->colitions[((y - 1) * 30) + x];
+            x2++;
+        }
+        y2++;   
+    }
+
+    int tile = tiles[((y / 64) * 30) + ((x + 30) / 64)];
+    return tile;
 }
 
 
@@ -112,8 +135,8 @@ void Init(void)
     tileInfo.height = 16;
 
     LoadMapFromFile("./assets/maps/test1.map", &map0);
-    LoadMapFromFile("./assets/maps/mapa1.map", &map1);
-    LoadMapFromFile("./assets/maps/mapa2.map", &map2);
+    LoadMapFromFile("./assets/maps/test2.map", &map1);
+    LoadMapFromFile("./assets/maps/test3.map", &map2);
     actualMap = &map0;
 }
 
@@ -199,15 +222,13 @@ void InputHandler(HWND hwnd)
 void Update(void)
 {
     mago.row = (int)timePass % magoNumFrames;
-    //tileInfo.x += (mago.x - magoPos.x);
-    //tileInfo.y += (mago.y - magoPos.y);
      
     tileInfo.x += (mago.x - magoPos.x);
     tileInfo.y += (mago.y - magoPos.y);
     if(tileInfo.x > 0 || mago.x < (windowWidth / 2) - 32)
     {
         tileInfo.x = 0;
-        mago.x -= (mago.x - magoPos.x);
+        mago.x -= (mago.x - magoPos.x); 
     }
     
     if(tileInfo.x + (30 * (16 * 4)) < windowWidth || mago.x > (windowWidth / 2) + 32)
@@ -230,20 +251,45 @@ void Update(void)
     }
     
     if(ChangeMap(mago.x, mago.y))
-    {
-        actualMap = &map1;
+    { 
+        if(actualMapInt == 1)
+        {
+            if(GetColitionsInt(mago.x, mago.y) == 3)
+            {
+                actualMapInt = 3;
+                actualMap = &map2;
+                mago.y = 128;
+            }
+            else
+            {
+                actualMapInt = 2;
+                actualMap = &map1;
+                mago.y = 64;
+                tileInfo.x += 128;
+            }
+        }
+        else if(actualMapInt == 3)
+        {
+            actualMapInt = 1;
+            actualMap = &map0;
+            mago.y = (windowHeight / 2) - 32;
+            tileInfo.y = -384;
+        }
+        else if(actualMapInt == 2)
+        {
+            actualMapInt = 1;
+            actualMap = &map0;
+            mago.y = windowHeight - 64;
+            tileInfo.x -= 128;
+        } 
     }
 }
 
+
 void Render(win32BackBuffer_t* backBuffer, HWND hwnd)
 {
-    //DrawTexture(0, 0, &backgroundHeader, backgroundTexture, backBuffer);
     DrawTileMapInt(10, 10, actualMap->bottomLayer, tileInfo, 4, tilesheetTexture, backBuffer);
     DrawFrameTexture(mago, magoScale, magoTexture, backBuffer);
-    //DrawRect((mago.x + 20), (mago.y), 4, 4, 0xFFFF00FF, backBuffer);
-    //DrawRect((mago.x + 40), (mago.y), 4, 4, 0xFFFF00FF, backBuffer);
-    //DrawRect((mago.x + 20), (mago.y + 40), 4, 4, 0xFFFF00FF, backBuffer);
-    //DrawRect((mago.x + 40), (mago.y + 40), 4, 4, 0xFFFF00FF, backBuffer);
     DrawTileMapInt(10, 10, actualMap->topLayer, tileInfo, 4, tilesheetTexture, backBuffer);
     DrawString("POKEMON SOUL SILVER", 0, windowHeight - 16, fontTexture, backBuffer);
     DrawString("AGUANTE LUGIA PAPA", 0, windowHeight - 32, fontTexture, backBuffer);
@@ -290,6 +336,3 @@ int CALLBACK WinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance, LPSTR lpCmdLi
     }
     return(0); 
 }
-
-
-
